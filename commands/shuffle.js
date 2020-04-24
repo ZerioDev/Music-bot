222f1dc4627c16ae144a9d1d3bece8ed3597d64e
Androz2091 <androz2091@gmail.com>
2020-04-24 17:10:14 +0200

    Add shuffle command

---
+++ commands/shuffle.js
@@ -0,0 +1,17 @@
+const emotes = require ("../config/emojis.json");
+
+module.exports.run = async (client, message) => {
+
+    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`);
+
+    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`);
+
+    client.player.shuffle(message.guild.id);
+    return message.channel.send(`Queue shuffled ${emotes.success}`);
+    
+};
+
+module.exports.config = {
+    name: "shuffle",
+    aliases: []
+};
