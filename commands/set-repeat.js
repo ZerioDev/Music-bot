18a4cc52c410f842ffab6f9b214f7e67d4b84f1e
Androz2091 <androz2091@gmail.com>
2020-04-24 16:59:00 +0200

    Add set-repeat command

---
+++ commands/set-repeat.js
@@ -0,0 +1,24 @@
+const emotes = require ("../config/emojis.json");
+
+module.exports.run = async (client, message) => {
+
+    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`);
+
+    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`);
+
+    const repeatMode = client.player.getQueue(message.guild.id).repeatMode;
+
+    if(repeatMode){
+        client.player.setRepeatMode(message.guild.id, false);
+        return message.channel.send(`Repeat mode disabled ${emotes.success}`);
+    } else {
+        client.player.setRepeatMode(message.guild.id, true);
+        return message.channel.send(`Repeat mode enabled ${emotes.success}`);
+    }
+    
+};
+
+module.exports.config = {
+    name: "set-repeat",
+    aliases: []
+};
