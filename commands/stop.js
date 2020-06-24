e6785f0b50ca7f0d9bb8bf08710b519e03187e89
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-06-24 15:04:16 +0200

    📝 Modifications handler / code

--- commands/stop.js
+++ commands/stop.js
@@ -1,18 +1,18 @@
 const emotes = require ("../config/emojis.json");
+const Discord = require("discord.js")
 
-module.exports.run = async (client, message) => {
+exports.run = async (client, message, args) => {
 
-    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`);
+    //If the member is not in a voice channel
+    if(!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
 
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`);
+    //If there's no music
+    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`No music playing on this server ${emotes.error}`);
 
+    //Stop player
     client.player.stop(message.guild.id);
 
-    message.channel.send(`**Music stopped ${emotes.success}**`);
+    //Message
+    message.channel.send(`Music stopped ${emotes.success}`);
 
-};
-
-module.exports.config = {
-    name: "stop",
-    aliases: []
-};
+}
