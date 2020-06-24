1bd779b25690b22cd017be8f9fe335d51722a3b2
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-06-24 14:56:45 +0200

    📝 Modifications handler / code

--- commands/now-playing.js
+++ commands/now-playing.js
@@ -1,18 +1,17 @@
 const emotes = require ("../config/emojis.json");
+const Discord = require("discord.js")
 
-module.exports.run = async (client, message) => {
+exports.run = async (client, message, args) => {
 
-    if(!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`);
+    //If the member is not in a voice channel
+    if(!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
 
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`);
+    //If there's no music
+    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`No music playing on this server ${emotes.error}`);
 
-    const track = await client.player.nowPlaying(message.guild.id);
+    const song = await client.player.nowPlaying(message.guild.id);
 
-    message.channel.send(`**Currently playing ${track.name} ${emotes.music}**`);
+    //Message
+    message.channel.send(`Currently playing ${song.name} ${emotes.music}\nProgression : [${client.player.createProgressBar(message.guild.id)}]`);
 
-};
-
-module.exports.config = {
-    name: "now-playing",
-    aliases: []
-};
+}
