09483a5eda95d8e4f6c1cb51b9cebece1bea986e
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-06-24 15:06:54 +0200

    ⌛️ Add events

---
+++ events/message.js
@@ -0,0 +1,27 @@
+const config = require(`../config/bot.json`)
+
+module.exports = (client, message) => {
+
+    //Ignore all bots
+    if (message.author.bot) return;
+
+    //Prefix
+    let prefix = config.prefix
+
+    //Ignore messages not starting with the prefix (in config.json)
+    if (message.content.indexOf(prefix) !== 0) return;
+  
+    //Our standard argument/command name definition.
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+  
+    //Grab the command data from the client.commands Enmap
+    const cmd = client.commands.get(command);
+  
+    //If that command doesn't exist, silently exit and do nothing
+    if (!cmd) return;
+  
+    //Run the command
+    cmd.run(client, message, args);
+
+};
