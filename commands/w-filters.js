e89cae3bf6f040d9bb62f541d7ee453f2639ecee
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-06-24 15:04:57 +0200

    📝 Modifications handler / code

--- commands/w-filters.js
+++ commands/w-filters.js
@@ -1,13 +1,17 @@
-const emotes = require("../config/emojis.json");
-const filters = require("../config/filters.json");
-const Discord = require("discord.js");
+const config = require ("../config/bot.json");
+const emotes = require ("../config/emojis.json");
+const filters = require ("../config/filters.json");
+const Discord = require("discord.js")
 
-module.exports.run = async (client, message) => {
+exports.run = async (client, message, args) => {
 
-    if (!message.member.voice.channel) return message.channel.send(`**You're not in a voice channel ${emotes.error}**`);
+    //If the member is not in a voice channel
+    if(!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
 
-    if (!client.player.isPlaying(message.guild.id)) return message.channel.send(`**No music playing on this server ${emotes.error}**`);
+    //If there's no music
+    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`No music playing on this server ${emotes.error}`);
 
+    //Emojis
     const enabledEmoji = emotes.success;
     const disabledEmoji = emotes.error;
 
@@ -18,18 +22,14 @@ module.exports.run = async (client, message) => {
         array.push(filters[filterName] + " : " + (client.player.getQueue(message.guild.id).filters[filterName] ? enabledEmoji : disabledEmoji));
     });
 
+    //List embed
     const list = new Discord.MessageEmbed()
-    .setDescription("Liste de tous les filtres activés ou non.\nPour ajouter un filtre sur une musique `^filter`.")
-    .addField("**Filtres**", filtersStatuses[0].join('\n'), true)
+    .setDescription(`List of all filters enabled or disabled.\nTo add a filter to a \`${config.prefix}filter\` music.`)
+    .addField("**Filters**", filtersStatuses[0].join('\n'), true)
     .addField("** **", filtersStatuses[1].join('\n'), true)
-    .addField("** **", `Attention, sur la commande \`^filter\`.\nLe nom du filtre doit être en minuscule.`)
     .setColor("ORANGE");
 
+    //Message
     message.channel.send(list);
 
-};
-
-module.exports.config = {
-    name: "w-filters",
-    aliases: []
-};
+}
