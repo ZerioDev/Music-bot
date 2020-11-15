3a69b0203f50165ffc9d7a0f03ad73f87834cef2
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-15 20:12:00 +0100

    📝 Correction

--- commands/queue.js
+++ commands/queue.js
@@ -10,6 +10,6 @@ exports.run = async (client, message) => {
 
     message.channel.send(`**Server queue - ${message.guild.name} ${emotes.queue}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
         return `**#${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`
-    }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`));
+    }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`));
 
 };
