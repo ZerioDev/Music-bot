a628747ede2015963e696a8e1b56eccd71257e3d
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-15 18:51:29 +0100

    ✨ Bug fixes & update (v2.0.0)

--- commands/loop.js
+++ commands/loop.js
@@ -1,33 +1,19 @@
-const emotes = require ("../config/emojis.json");
-const Discord = require("discord.js")
+const emotes = require("../config/emojis.json");
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, message) => {
 
-    //If the member is not in a voice channel
-    if(!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
+    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
 
-    //If there's no music
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`No music playing on this server ${emotes.error}`);
+    if (!client.player.getQueue(message)) return message.channel.send(`No music playing on this server ${emotes.error}`);
 
-    //Repeat mode
-    const repeatMode = client.player.getQueue(message.guild.id).repeatMode;
+    const repeatMode = client.player.getQueue(message).repeatMode;
 
-    //If the mode is enabled
-    if(repeatMode) {
-
-        client.player.setRepeatMode(message.guild.id, false);
-
-        //Message
-        return message.channel.send(`Repeat mode disabled ${emotes.success}`);
-
-    //If the mode is disabled
+    if (repeatMode) {
+        client.player.setRepeatMode(message, false);
+        return message.channel.send(`Repeat mode **disabled** ${emotes.success}`);
     } else {
+        client.player.setRepeatMode(message, true);
+        return message.channel.send(`Repeat mode **enabled** ${emotes.success}`);
+    };
 
-        client.player.setRepeatMode(message.guild.id, true);
-
-        //Message
-        return message.channel.send(`Repeat mode enabled ${emotes.success}`);
-
-    }
-    
-}
+};
