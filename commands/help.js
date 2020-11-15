7def900256523a25958cf3295f14e08fc447fd41
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-15 18:51:43 +0100

    ✨ Bug fixes & update (v2.0.0)

--- commands/help.js
+++ commands/help.js
@@ -1,19 +1,20 @@
-const config = require ("../config/bot.json");
-const emotes = require ("../config/emojis.json");
-const Discord = require("discord.js")
+const config = require("../config/bot.json");
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, message) => {
 
-    //New embed
-    const help = new Discord.MessageEmbed()
-    .setDescription("Find the list of available commands on this panel.")
-    .addField("**Music - (11)**", "`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `np`, `loop`, `volume`, `skip`, `stop`")
-    .addField("**Filters - (18)**", "`bassboost`, `8D`, `vaporwave`, `nightcore`, `phaser`, `tremolo`, `vibrato`, `reverse`, `treble`, `normalizer`, `surrounding`, `pulsator`, `subboost`, `karaoke`, `flanger`, `gate`, `haas`, `mcompand`")
-    .addField("**Informations - (1)**", "`ping`")
-    .setFooter(`To use filters, ${config.prefix}filter (the filter). Example: ${config.prefix}filter 8D.`)
-    .setColor("ORANGE")
+    message.channel.send({
+        embed: {
+            color: 'ORANGE',
+            author: { name: 'Help pannel' },
+            footer: { text: 'This bot uses a Github project made by Zerio (ZerioDev/Music-bot)' },
+            fields: [
+                { name: 'Bot', value: '`ping`' },
+                { name: 'Music', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `np`, `loop`, `volume`, `skip`, `stop`' },
+                { name: 'Filters', value: '`bassboost`, `8D`, `vaporwave`, `nightcore`, `phaser`, `tremolo`, `vibrato`, `reverse`, `treble`, `normalizer`, `surrounding`, `pulsator`, `subboost`, `karaoke`, `flanger`, `gate`, `haas`, `mcompand`' },
+            ],
+            timestamp: new Date(),
+            description: `To use filters, ${config.prefix}filter (the filter). Example : ${config.prefix}filter 8D.`,
+        },
+    });
 
-    //Message
-    message.channel.send(help)
-
-}
+};
