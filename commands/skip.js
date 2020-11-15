bea2f14eed83da760c5810e8c01c0cd37931f100
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-15 18:49:18 +0100

    ✨ Bug fixes & update (v2.0.0)

--- commands/skip.js
+++ commands/skip.js
@@ -1,17 +1,13 @@
-const emotes = require ("../config/emojis.json");
-const Discord = require("discord.js")
+const emotes = require("../config/emojis.json");
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, message) => {
 
-    //If the member is not in a voice channel
-    if(!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
+    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
 
-    //If there's no music
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`No music playing on this server ${emotes.error}`);
+    if (!client.player.getQueue(message)) return message.channel.send(`No songs currently playing ${emotes.error}`);
 
-    const track = await client.player.skip(message.guild.id);
+    client.player.skip(message);
 
-    //Message
-    message.channel.send(`Song ${track.name} skipped ${emotes.success}`);
+    message.channel.send(`The current music has just been **skipped** ${emotes.success}`);
 
-}
+};
