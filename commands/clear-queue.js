18cbf4443ac12441d5f2f32b9412a6078adfc537
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-15 18:52:28 +0100

    ✨ Bug fixes & update (v2.0.0)

--- commands/clear-queue.js
+++ commands/clear-queue.js
@@ -1,18 +1,13 @@
-const emotes = require ("../config/emojis.json");
-const Discord = require("discord.js")
+const emotes = require("../config/emojis.json");
 
-exports.run = async (client, message, args) => {
-  
-    //If the member is not in a voice channel
-    if(!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
+exports.run = async (client, message) => {
 
-    //If there's no music
-    if(!client.player.isPlaying(message.guild.id)) return message.channel.send(`No music playing on this server ${emotes.error}`);
+    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
 
-    //The bot removes the waiting list
-    client.player.clearQueue(message.guild.id);
+    if (!client.player.getQueue(message)) return message.channel.send(`No songs currently playing ${emotes.error}`);
 
-    //Success message
-    message.channel.send(`Queue cleared ${emotes.success}`);
+    client.player.clearQueue(message);
 
-}
+    message.channel.send(`The queue has just been removed ${emotes.success}`);
+
+};
