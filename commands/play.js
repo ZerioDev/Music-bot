4b431b8673a12940029fe38dfdba73ad33469000
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-15 20:42:22 +0100

    📝 Correction

--- commands/play.js
+++ commands/play.js
@@ -4,6 +4,8 @@ exports.run = async (client, message, args) => {
 
     if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
 
+    if (!args[0]) return message.channel.send(`Please indicate the title of a song ${emotes.error}`);
+
     client.player.play(message, args.join(" "));
 
 };
