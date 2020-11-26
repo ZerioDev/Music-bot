109e4d130781050fcf1e766ec8cc2e70925b9ec7
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-26 22:18:09 +0100

    📝 Correction

--- commands/pause.js
+++ commands/pause.js
@@ -2,7 +2,7 @@ exports.run = async (client, message) => {
 
     if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-    if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No songs currently playing !`);
+    if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
 
     client.player.pause(message);
 
