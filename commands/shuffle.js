ec26c9813d0319e21f1c1e56917f0b76232fd6f6
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-26 22:14:52 +0100

    ✨ Update (v2.0.1)

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,13 +1,11 @@
-const emotes = require("../config/emojis.json");
-
 exports.run = async (client, message) => {
 
-    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
+    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-    if (!client.player.getQueue(message)) return message.channel.send(`No music playing on this server ${emotes.error}`);
+    if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No music currently playing !`);
 
     client.player.shuffle(message);
 
-    return message.channel.send(`Queue shuffled **${client.player.getQueue(message).tracks.length}** song(s) ${emotes.success}`);
+    return message.channel.send(`${client.emotes.success} - Queue shuffled **${client.player.getQueue(message).tracks.length}** song(s) !`);
 
 };
