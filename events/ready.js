a6061e98ca28869909c9121bde3a2edd8f1dc616
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-26 22:23:53 +0100

    ✨ Update (v2.0.1)

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,7 @@
-const config = require("../config/bot.json");
-
 module.exports = async (client) => {
 
     console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
 
-    client.user.setActivity(config.game)
+    client.user.setActivity(client.config.game)
 
 };
