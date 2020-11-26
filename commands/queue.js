ddc1e910cec9ac47264ad0df054fb70d6eaeca6f
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-26 22:14:21 +0100

    ✨ Update (v2.0.1)

--- commands/queue.js
+++ commands/queue.js
@@ -1,14 +1,12 @@
-const emotes = require("../config/emojis.json");
-
 exports.run = async (client, message) => {
 
-    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
+    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
     const queue = client.player.getQueue(message);
 
-    if (!queue) return message.channel.send(`No songs currently playing ${emotes.error}`);
+    if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No songs currently playing !`);
 
-    message.channel.send(`**Server queue - ${message.guild.name} ${emotes.queue}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
+    message.channel.send(`**Server queue - ${message.guild.name} ${client.emotes.queue}**\nCurrent : ${queue.playing.title} | ${queue.playing.author}\n\n` + (queue.tracks.map((track, i) => {
         return `**#${i + 1}** - ${track.title} | ${track.author} (requested by : ${track.requestedBy.username})`
     }).slice(0, 5).join('\n') + `\n\n${queue.tracks.length > 5 ? `And **${queue.tracks.length - 5}** other songs...` : `In the playlist **${queue.tracks.length}** song(s)...`}`));
 
