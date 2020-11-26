a021632e3d81c3672f1b26d9f5753eaf07f79672
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-26 22:10:24 +0100

    ✨ Update (v2.0.1)

--- commands/clear-queue.js
+++ commands/clear-queue.js
@@ -1,13 +1,11 @@
-const emotes = require("../config/emojis.json");
-
 exports.run = async (client, message) => {
 
-    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
+    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-    if (!client.player.getQueue(message)) return message.channel.send(`No songs currently playing ${emotes.error}`);
+    if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No songs currently playing !`);
 
     client.player.clearQueue(message);
 
-    message.channel.send(`The queue has just been removed ${emotes.success}`);
+    message.channel.send(`${client.emotes.success} - The queue has just been **removed** !`);
 
 };
