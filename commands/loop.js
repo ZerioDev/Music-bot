ad5dcfd6147e9ce1b4d925a44fe3051b0832466d
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-26 22:11:26 +0100

    ✨ Update (v2.0.1)

--- commands/loop.js
+++ commands/loop.js
@@ -1,19 +1,17 @@
-const emotes = require("../config/emojis.json");
-
 exports.run = async (client, message) => {
 
-    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
+    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-    if (!client.player.getQueue(message)) return message.channel.send(`No music playing on this server ${emotes.error}`);
+    if (!client.player.getQueue(message)) return message.channel.send(`${client.emotes.error} - No songs currently playing !`);
 
     const repeatMode = client.player.getQueue(message).repeatMode;
 
     if (repeatMode) {
         client.player.setRepeatMode(message, false);
-        return message.channel.send(`Repeat mode **disabled** ${emotes.success}`);
+        return message.channel.send(`${client.emotes.success} - Repeat mode **disabled** !`);
     } else {
         client.player.setRepeatMode(message, true);
-        return message.channel.send(`Repeat mode **enabled** ${emotes.success}`);
+        return message.channel.send(`${client.emotes.success} - Repeat mode **enabled** !`);
     };
 
 };
