de65bc786bdcb10ccfdd8a4fc9526bc781c64e66
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-26 22:12:27 +0100

    ✨ Update (v2.0.1)

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,5 @@
-const emotes = require("../config/emojis.json");
-
 exports.run = async (client, message) => {
 
-    message.channel.send(`Ping : **${client.ws.ping}ms** ${emotes.success}`)
+    message.channel.send(`${client.emotes.success} - Ping : **${client.ws.ping}ms** !`)
 
 };
