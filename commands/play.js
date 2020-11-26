fbbaa553525f3cd6dc6d44dc2404006b7c8a68c5
Zerio <43386412+ZerioDev@users.noreply.github.com>
2020-11-26 22:12:55 +0100

    ✨ Update (v2.0.1)

--- commands/play.js
+++ commands/play.js
@@ -1,10 +1,8 @@
-const emotes = require("../config/emojis.json");
-
 exports.run = async (client, message, args) => {
 
-    if (!message.member.voice.channel) return message.channel.send(`You're not in a voice channel ${emotes.error}`);
+    if (!message.member.voice.channel) return message.channel.send(`${client.emotes.error} - You're not in a voice channel !`);
 
-    if (!args[0]) return message.channel.send(`Please indicate the title of a song ${emotes.error}`);
+    if (!args[0]) return message.channel.send(`${client.emotes.error} - Please indicate the title of a song !`);
 
     client.player.play(message, args.join(" "));
 
