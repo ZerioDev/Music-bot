2e9acecbdfce1d42045d351f0fac8b08fbfbbd12
Androz2091 <androz2091@gmail.com>
2021-02-26 13:40:55 +0100

    :sparkles: Add new video unavailable error

--- player/error.js
+++ player/error.js
@@ -1,4 +1,4 @@
-module.exports = (client, error, message) => {
+module.exports = (client, error, message, ...args) => {
     switch (error) {
         case 'NotPlaying':
             message.channel.send(`${client.emotes.error} - There is no music being played on this server !`);
@@ -8,6 +8,9 @@ module.exports = (client, error, message) => {
             break;
         case 'UnableToJoin':
             message.channel.send(`${client.emotes.error} - I am not able to join your voice channel, please check my permissions !`);
+            break;
+        case 'VideoUnavailable':
+            message.channel.send(`${client.emotes.error} - ${args[0].title} is not available in your country! Skipping...`);
             break;
         default:
             message.channel.send(`${client.emotes.error} - Something went wrong ... Error : ${error}`);
