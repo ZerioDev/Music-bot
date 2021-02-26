467bc12ffb13c36b97287859b45a95adb78f3c00
Androz2091 <androz2091@gmail.com>
2021-02-26 13:41:07 +0100

    :pencil: Add client username in ready

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = async (client) => {
-    console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
+    console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
 
     client.user.setActivity(client.config.discord.activity);
 };
