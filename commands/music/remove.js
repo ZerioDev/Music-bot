6a2357a025a2fc7bd79669390164442fcec4af69
Dries1234 <driesolbrechts@gmail.com>
2021-09-26 18:10:20 +0200

    error handling

--- commands/music/remove.js
+++ commands/music/remove.js
@@ -10,6 +10,8 @@ module.exports = {
         if (!queue || !queue.playing) return message.channel.send(`No music currently playing ${message.author}... try again ? ❌`);
 
         if (!queue.tracks[0]) return message.channel.send(`No music in the queue after the current one ${message.author}... try again ? ❌`);
+        if(args[0] > queue.tracks.length || args[0] < 1) return message.channel.send("That is not a valid track!... try again ? ❌")
+        
         queue.tracks.splice(args[0] - 1, 1)
 
         message.channel.send(`Removed song at queue position ${args[0]} 🗑️`);
