0f5119de2ef911807d88faca9bbf5bfe0722fba4
callum <86750799+callumbash@users.noreply.github.com>
2021-10-07 16:49:46 +0100

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,7 +1,7 @@
 module.exports = (client, message) => {
     if (message.author.bot || message.channel.type === 'dm') return;
 
-    const prefix = client.config.app.prefix;
+    const prefix = client.config.app.px;
 
     if (message.content.indexOf(prefix) !== 0) return;
 
