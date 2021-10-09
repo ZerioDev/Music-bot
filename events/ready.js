a9b8c13021f3ef21fc6d31f117fab9b7ef4bd8a7
revot334 <78510325+revot334@users.noreply.github.com>
2021-10-09 13:13:32 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,15 @@
 module.exports = async (client) => {
-    console.log(`Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`);
+   
 
-    client.user.setActivity(client.config.app.playing);
-};
+         let guilds = client.guilds.cache.map(guild => guild.id);
+         console.log(`Discord Info:`)
+         console.log(`Total Users ${client.users.cache.size}`);
+         console.log(`Total Channels ${client.channels.cache.size}`);
+         console.log(`Total Guilds ${client.guilds.cache.size}`);
+         console.log(`Guild id's ${guilds}`)
+         console.log('bot status', client.user.presence.status);
+         console.log(`${client.user.username} is Ready!`);
+         console.log('Made with love by ZerioDEV');
+        
+        client.user.setActivity(client.config.app.playing);
+        };
