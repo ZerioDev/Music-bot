847a357a6155b8be6c4bfe291e8ddc2cda5a7742
Zerio <43386412+ZerioDev@users.noreply.github.com>
2021-10-20 22:10:36 +0200

    ⚡️ Ephemeral message

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -5,12 +5,12 @@ module.exports = (client, int) => {
 
     switch (int.customId) {
         case 'saveTrack': {
-            if (!queue || !queue.playing) return int.reply({ content: `No music currently playing <@${int.user.id}>... try again ? ❌`, components: [] });
+            if (!queue || !queue.playing) return int.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true, components: [] });
 
             int.member.send(`You saved the track ${queue.current.title} | ${queue.current.author} from the server ${int.member.guild.name} ✅`).then(() => {
-                return int.reply({ content: `I have sent you the title of the music by private messages <@${int.user.id}> ✅`, components: [] });
+                return int.reply({ content: `I have sent you the title of the music by private messages ✅`, ephemeral: true, components: [] });
             }).catch(error => {
-                return int.reply({ content: `Unable to send you a private message <@${int.user.id}>... try again ? ❌`, components: [] });
+                return int.reply({ content: `Unable to send you a private message... try again ? ❌`, ephemeral: true, components: [] });
             });
         }
     }
