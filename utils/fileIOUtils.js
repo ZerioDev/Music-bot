6871c2a2d9bb6035a90c42b2b9bd2fcbfeb885c3
IqrarAminullah <Iqrar.silveria@gmail.com>
2021-11-11 18:41:09 +0700

    creating branch ( i forgot XD) also adding new functions to fileIO utils

--- utils/fileIOUtils.js
+++ utils/fileIOUtils.js
@@ -4,12 +4,13 @@ var getDirName = require('path').dirname;
 
 const projectRoot = __dirname + "/../" //__dirname points to this file location
 
-
+//write file to path relative from project root
 module.exports.writeFile = function(filePath, contents, cb) {
   var relativePath = path.join(projectRoot + filePath)
   fs.mkdir(getDirName(relativePath), { recursive: true}, function (err) {
     if (err) return cb("[File IO error] "+ err);
-
+  
+    //Check if file exist
     if(fs.existsSync(relativePath)){
       return cb("Already Exist");
     }else{
@@ -18,11 +19,41 @@ module.exports.writeFile = function(filePath, contents, cb) {
   });
 }
 
+//read file to path relative from project root
 module.exports.readFile = function(filePath){
   var relativePath = path.join(projectRoot + filePath)
   return fs.readFileSync(relativePath);
 }
 
+//read json file
 module.exports.readJsonFile = function(filePath){
   return JSON.parse(this.readFile(filePath +".json"));
+}
+
+//delete file
+module.exports.deleteFile = function(filePath, cb){
+  var relativePath = path.join(projectRoot + filePath);
+  
+  //check if file exist
+  if(!fs.existsSync(relativePath)){
+    return cb("Not Exists");
+  }
+
+  fs.unlink(relativePath, function(err){
+    if(err) return cb("[File IO Error]" + err);
+  });
+}
+
+module.exports.listDir = function(dirPath, cb){
+  var relativePath = path.join(projectRoot + dirPath)
+
+  if(!fs.existsSync(relativePath)){
+    return cb("Not Exist");
+  }
+
+  var files = fs.readdirSync(dirPath, function(err){
+    return cb("[File IO Error]" + err);
+  });
+
+  return files;
 }
