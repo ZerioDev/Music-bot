5510a377120660262b93aab19ae8ee69ae0b163b
Nimbi <JoshuaVL1988@protonmail.com>
2022-06-25 16:53:57 -0700

    :recycle: Update env

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
+let PREFIX = process.env.CLIENT_PREFIX;
+
 module.exports = async (client) => {
     console.log(`Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`);
 
-    client.user.setActivity(client.config.app.playing);
+    client.user.setActivity(`${PREFIX}help in ${client.guilds.cache.size} servers`);
 };
