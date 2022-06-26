4233fc3a36f896f42fd887bec89de516cf9cf724
Nimbi <JoshuaVL1988@protonmail.com>
2022-06-25 17:09:14 -0700

    :recycle: Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 let PREFIX = process.env.CLIENT_PREFIX;
 
 module.exports = async (client) => {
-    console.log(`Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`);
+    console.log([
+        `--------------------------------------------------`,
+        `Logged in as ${client.user.username}`,
+        `--------------------------------------------------`,
+        `-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`,
+        `--------------------------------------------------`
+    ].join('\n'))
+
 
     client.user.setActivity(`${PREFIX}help in ${client.guilds.cache.size} servers`);
 };
