2b6f5d95bba17ca7aae752826154627a281e47e5
chemical_tourist_18 <root@tourist>
2022-11-26 20:52:56 +0000

    Defer replies for long-running commands

--- commands/music/playnext.js
+++ commands/music/playnext.js
@@ -14,10 +14,11 @@ module.exports = {
         }
     ],
 
-    async execute({ inter }) { 
+    async execute({ inter }) {
+	inter.deferReply();
         const queue = player.getQueue(inter.guildId);
 
-        if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
+        if (!queue || !queue.playing) return inter.editReply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
 
         const song = inter.options.getString('song');
 
@@ -26,13 +27,13 @@ module.exports = {
             searchEngine: QueryType.AUTO
         });
 
-        if (!res || !res.tracks.length) return inter.reply({ content: `No results found ${inter.member}... try again ? ❌`, ephemeral: true });
+        if (!res || !res.tracks.length) return inter.editReply({ content: `No results found ${inter.member}... try again ? ❌`, ephemeral: true });
 
-       if (res.playlist) return inter.reply({ content: `This command dose not support playlist's ${inter.member}... try again ? ❌`, ephemeral: true });
+       if (res.playlist) return inter.editReply({ content: `This command dose not support playlist's ${inter.member}... try again ? ❌`, ephemeral: true });
 
         queue.insert(res.tracks[0], 0)
 
-        await inter.reply({ content:`Track has been inserted into the queue... it will play next 🎧`});
+        await inter.editReply({ content:`Track has been inserted into the queue... it will play next 🎧`});
 
     }
 }
