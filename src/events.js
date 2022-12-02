116fbd318c25994d692f1e7ee74733dc0944dd05
Monckey100 <Lf2owner@hotmail.com>
2022-12-01 19:29:53 -0800

    toggle

--- src/events.js
+++ src/events.js
@@ -62,11 +62,15 @@ player.on('channelEmpty', (queue) => {
 client.on('voiceStateUpdate', (oldState, newState) => {
     let newUserChannel = oldState.channel
     let oldUserChannel = newState.channel
+    console.log(newUserChannel.members.size)
     if(newUserChannel == undefined && oldUserChannel == undefined) return; // Not a join or a leave state
     if(oldUserChannel == undefined && newUserChannel.members.size == 2) { // Someone joined
         // User Joins a voice channel
         const queue = player.getQueue(newUserChannel.guild.id); // Grab queue
-       if(queue.connection.paused) queue.setPaused(false) // Unpause the song
+       if(queue.connection.paused) { 
+        queue.setPaused(true)
+        queue.setPaused(false) // Unpause the song
+       }
     } else if(newUserChannel === undefined){  // someone left
       // User leaves a voice channel
   
