ebc6c4d3ed2e9690e1578322de4197c6a087fa13
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-01-01 15:20:29 +0100

    Updated SAVE command : disabled in bot's DMs

--- commands/music/save.js
+++ commands/music/save.js
@@ -4,6 +4,7 @@ module.exports = {
     name: 'save',
     description: 'save the current track!',
     voiceChannel: true,
+    dm_permission: false,
 
     async execute({ inter }) {
         const queue = player.getQueue(inter.guildId);
