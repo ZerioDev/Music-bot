7795e0290e38b145a01d9a00b677770a20b5ff34
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-03-06 21:20:53 +0100

    👽️Update play command
    Update due to changes in discord-player v6.

--- commands/music/play.js
+++ commands/music/play.js
@@ -13,8 +13,8 @@ module.exports = {
         }
     ],
 
-    async execute({ inter }) {
-	await inter.deferReply();
+    async execute({ inter, client }) {
+	    await inter.deferReply();
         const song = inter.options.getString('song');
         const res = await player.search(song, {
             requestedBy: inter.member,
@@ -23,11 +23,16 @@ module.exports = {
 
         if (!res || !res.tracks.length) return inter.editReply({ content: `No results found ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        const queue = await player.createQueue(inter.guild, {
-            metadata: inter.channel,
-            spotifyBridge: client.config.opt.spotifyBridge,
-            initialVolume: client.config.opt.defaultvolume,
-            leaveOnEnd: client.config.opt.leaveOnEnd
+        const queue = player.nodes.create(inter.guild, {
+            metadata: {
+                channel: inter.channel,
+                client: inter.guild.members.me,
+                requestedBy: inter.user,
+            },
+            selfDeaf: true,
+            volume: client.config.opt.defaultvolume,
+            leaveOnEmpty: client.config.opt.leaveOnEmpty,
+            leaveOnEnd: client.config.opt.leaveOnEnd,
         });
 
         try {
@@ -39,8 +44,8 @@ module.exports = {
 
        await inter.editReply({ content:`Loading your ${res.playlist ? 'playlist' : 'track'}... 🎧`});
 
-        res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
+        res.playlist ? queue.addTrack(res.tracks) : queue.addTrack(res.tracks[0]);
 
-        if (!queue.playing) await queue.play();
+        if (!queue.playing) await queue.node.play();
     },
 };
