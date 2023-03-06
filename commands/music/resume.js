d42f306d7f2d55750c79820968369174b530bab0
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-03-06 21:24:00 +0100

    👽️Update filter command
    Update due to changes in discord-player v6.

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -4,15 +4,15 @@ module.exports = {
     voiceChannel: true,
 
     execute({ inter }) {
-        const queue = player.getQueue(inter.guildId);
+        const queue = player.nodes.get(inter.guildId);
 
         if (!queue) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
         
 
-        if(!queue.connection.paused) return inter.reply({content: `The track is already running, ${inter.member}... try again ? ❌`, ephemeral: true})
+        if(queue.node.isPlaying()) return inter.reply({content: `The track is already running, ${inter.member}... try again ? ❌`, ephemeral: true})
 
-        const success = queue.setPaused(false);
+        const success = queue.node.resume();
         
-        return inter.reply({ content:success ? `Current music ${queue.current.title} resumed ✅` : `Something went wrong ${inter.member}... try again ? ❌`});
+        return inter.reply({ content:success ? `Current music ${queue.currentTrack.title} resumed ✅` : `Something went wrong ${inter.member}... try again ? ❌`});
     },
 };
