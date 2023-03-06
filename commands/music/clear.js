124e851863c9a4de533365c05e22f100fe42ca94
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-03-06 21:15:11 +0100

    👽️Update clear command
    Update due to changes in discord-player v6.

--- commands/music/clear.js
+++ commands/music/clear.js
@@ -4,13 +4,13 @@ module.exports = {
     voiceChannel: true,
 
     async execute({ inter }) {
-        const queue = player.getQueue(inter.guildId);
+        const queue = player.nodes.get(inter.guildId);
 
         if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
 
         if (!queue.tracks[0]) return inter.reply({ content: `No music in the queue after the current one ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        await queue.clear();
+        await queue.tracks.clear();
 
         inter.reply(`The queue has just been cleared 🗑️`);
     },
