950cb4d349f1200af8451ebf4d0c2ab96e0dce8e
iGameCreep <110474721+iGameCreep@users.noreply.github.com>
2023-03-06 21:20:10 +0100

    👽️Update pause command
    Update due to changes in discord-player v6.

--- commands/music/pause.js
+++ commands/music/pause.js
@@ -4,16 +4,14 @@ module.exports = {
     voiceChannel: true,
 
     execute({ inter }) {
-        const queue = player.getQueue(inter.guildId);
+        const queue = player.nodes.get(inter.guildId);
 
         if (!queue) return inter.reply({ content: `No music currently playing ${inter.member}... try again ? ❌`, ephemeral: true });
-        
-        if(queue.connection.paused) return inter.reply({content: 'The track is currently paused!', ephemeral: true})
 
-        if(queue.connection.paused) return inter.reply({content: `The track is currently paused, ${inter.member}... try again ? ❌`, ephemeral: true})
+        if(queue.node.isPaused()) return inter.reply({content: `The track is currently paused, ${inter.member}... try again ? ❌`, ephemeral: true})
 
-        const success = queue.setPaused(true);
+        const success = queue.node.pause(true);
         
-        return inter.reply({ content: success ? `Current music ${queue.current.title} paused ✅` : `Something went wrong ${inter.member}... try again ? ❌` });
+        return inter.reply({ content: success ? `Current music ${queue.currentTrack.title} paused ✅` : `Something went wrong ${inter.member}... try again ? ❌` });
     },
 };
