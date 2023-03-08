942a6efaa8305c54f51fe934317165bfbc56e1b9
GameCreep35 <110474721+iGameCreep@users.noreply.github.com>
2023-03-08 18:24:52 +0100

    👽️Update savetrack button
    Update due to changes in discord-player v6.

--- src/buttons/savetrack.js
+++ src/buttons/savetrack.js
@@ -1,28 +1,27 @@
 const { EmbedBuilder } = require('discord.js')
 
 module.exports = async ({ inter, queue }) => {
-    if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
+    if (!queue || !queue.isPlaying()) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
 
     inter.member.send({
         embeds: [
             new EmbedBuilder()
-                .setColor('Red')
-                .setTitle(`:arrow_forward: ${queue.current.title}`)
-                .setURL(queue.current.url)
-                .addFields(
-                    { name: ':hourglass: Duration:', value: `\`${queue.current.duration}\``, inline: true },
-                    { name: 'Song by:', value: `\`${queue.current.author}\``, inline: true },
-                    { name: 'Views :eyes:', value: `\`${Number(queue.current.views).toLocaleString()}\``, inline: true },
-                    { name: 'Song URL:', value: `\`${queue.current.url}\`` }
-                )
-                .setThumbnail(queue.current.thumbnail)
-                .setFooter({ text: `from the server ${inter.member.guild.name}`, iconURL: inter.member.guild.iconURL({ dynamic: false }) })
+            .setColor('Red')
+            .setTitle(`:arrow_forward: ${queue.currentTrack.title}`)
+            .setURL(queue.currentTrack.url)
+            .addFields(
+                { name: ':hourglass: Duration:', value: `\`${queue.currentTrack.duration}\``, inline: true },
+                { name: 'Song by:', value: `\`${queue.currentTrack.author}\``, inline: true },
+                { name: 'Views :eyes:', value: `\`${Number(queue.currentTrack.views).toLocaleString()}\``, inline: true },
+                { name: 'Song URL:', value: `\`${queue.currentTrack.url}\`` }
+            )
+            .setThumbnail(queue.currentTrack.thumbnail)
+            .setFooter({ text: `from the server ${inter.member.guild.name}`, iconURL: inter.member.guild.iconURL({ dynamic: false }) })
         ]
     }).then(() => {
         return inter.reply({ content: `I have sent you the title of the music by private messages ✅`, ephemeral: true });
     }).catch(error => {
         return inter.reply({ content: `Unable to send you a private message... try again ? ❌`, ephemeral: true });
     });
-
 
 }
