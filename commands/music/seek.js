c435fd7918a7caad82f561431f5e3ba66063005f
GameCreep35 <110474721+iGameCreep@users.noreply.github.com>
2023-03-08 18:13:19 +0100

    👽️Update seek command
    Update due to changes in discord-player v6.

--- commands/music/seek.js
+++ commands/music/seek.js
@@ -14,15 +14,19 @@ module.exports = {
     }
     ],
     async execute({ inter }) {
-        const queue = player.getQueue(inter.guildId);
+        const queue = player.nodes.get(inter.guildId);
 
-        if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing ${inter.reply}... try again ? ❌`, ephemeral: true });
+        if (!queue || !queue.isPlaying()) return inter.reply({ content: `No music currently playing ${inter.reply}... try again ? ❌`, ephemeral: true });
 
         const timeToMS = ms(inter.options.getString('time'));
 
-        if (timeToMS >= queue.current.durationMS) return inter.reply({ content:`The indicated time is higher than the total time of the current song ${inter.member}... try again ? ❌\n*Try for example a valid time like **5s, 10s, 20 seconds, 1m**...*`, ephemeral: true });
+        if (timeToMS >= queue.currentTrack.durationMS) return inter.reply({ content:`The indicated time is higher than the total time of the current song ${inter.member}... try again ? ❌\n*Try for example a valid time like **5s, 10s, 20 seconds, 1m**...*`, ephemeral: true });
 
-        await queue.seek(timeToMS);
+        await queue.node.seek(timeToMS);
+
+        /*
+            Seek function is currently broken, waiting on some fix from the devs...
+        */
 
         inter.reply({ content: `Time set on the current song **${ms(timeToMS, { long: true })}** ✅`});
     },
