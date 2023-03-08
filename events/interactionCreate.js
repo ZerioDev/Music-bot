f4768bd3f8d9bbbd0a30cc9c0adf652931c5c0a2
GameCreep35 <110474721+iGameCreep@users.noreply.github.com>
2023-03-08 18:29:29 +0100

    👽️Update interactionCreate event
    Update due to changes in discord-player v6.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -17,7 +17,7 @@ module.exports = (client, inter) => {
     if (inter.type === InteractionType.MessageComponent) {
         const customId = JSON.parse(inter.customId)
         const file_of_button = customId.ffb
-        const queue = player.getQueue(inter.guildId);
+        const queue = player.nodes.get(inter.guildId);
         if (file_of_button) {
             delete require.cache[require.resolve(`../src/buttons/${file_of_button}.js`)];
             const button = require(`../src/buttons/${file_of_button}.js`)
