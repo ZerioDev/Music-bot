a6d9086aba1b8bfa4cc0e91d52b8c13f1c9afb6a
GameCreep35 <110474721+iGameCreep@users.noreply.github.com>
2023-03-08 18:26:53 +0100

    👽️Update volumedown button
    Update due to changes in discord-player v6.

--- src/buttons/volumedown.js
+++ src/buttons/volumedown.js
@@ -1,15 +1,19 @@
 const maxVol = client.config.opt.maxVol;
 
 module.exports = async ({  inter, queue }) => { 
-    if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
+    if (!queue || !queue.isPlaying()) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
 
-        const vol = Math.floor(queue.volume - 5)
+    const vol = Math.floor(queue.node.volume - 5)
 
-        if (vol < 0 ) return inter.reply({ content: `I can not move the volume down any more ${inter.member}... try again ? ❌`, ephemeral: true })
-        
-        if (queue.volume === vol) return inter.reply({ content: `The volume you want to change is already the current one ${inter.member}... try again ? ❌`, ephemeral: true });
+    if (vol < 0 ) return inter.reply({ content: `I can not move the volume down any more ${inter.member}... try again ? ❌`, ephemeral: true })
+    
+    if (queue.node.volume === vol) return inter.reply({ content: `The volume you want to change is already the current one ${inter.member}... try again ? ❌`, ephemeral: true });
 
-        const success = queue.setVolume(vol);
+    try {
+        queue.node.setVolume(vol);
+    } catch {
+        return inter.reply({ content: `Something went wrong ${inter.member}... try again ? ❌`, ephemeral: true});
+    }
 
-        return inter.reply({ content:success ? `The volume has been modified to **${vol}**/**${maxVol}**% 🔊` : `Something went wrong ${inter.member}... try again ? ❌`, ephemeral: true});
+    return inter.reply({ content: `The volume has been modified to **${vol}**/**${maxVol}**% 🔊`, ephemeral: true});
 }
