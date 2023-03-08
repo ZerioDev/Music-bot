7ede8527e31fb1100f43f5766f553caf1ae83fce
GameCreep35 <110474721+iGameCreep@users.noreply.github.com>
2023-03-08 18:26:20 +0100

    👽️Update skip button
    Update due to changes in discord-player v6.

--- src/buttons/skip.js
+++ src/buttons/skip.js
@@ -1,7 +1,7 @@
 module.exports = async ({  inter, queue }) => { 
-    if (!queue || !queue.playing) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
+    if (!queue || !queue.isPlaying()) return inter.reply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
     
-    const success = queue.skip();
+    const success = queue.node.skip();
 
-    return inter.reply({ content: success ? `Current music ${queue.current.title} skipped ✅` : `Something went wrong ${inter.member}... try again ? ❌`, ephemeral: true});
+    return inter.reply({ content: success ? `Current music ${queue.currentTrack.title} skipped ✅` : `Something went wrong ${inter.member}... try again ? ❌`, ephemeral: true});
 }
