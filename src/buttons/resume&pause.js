5b4edcd02335032e93ac7c5d2945287a7e8fb1e7
Niek Vlam <vlam4695@gmail.com>
2023-04-10 01:10:53 +0200

    Swapped resume/pause messages

--- src/buttons/resume&pause.js
+++ src/buttons/resume&pause.js
@@ -1,10 +1,15 @@
-module.exports = async ({  inter, queue }) => { 
+module.exports = async ({ inter, queue }) => {
     if (!queue || !queue.isPlaying()) return inter.editReply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
 
-    const success = queue.node.resume();
+    const resumed = queue.node.resume();
+    let message = `Current music ${queue.currentTrack.title} resumed ✅`;
     
-    if (!success) queue.node.pause();
-    
+    if (!resumed) {
+        queue.node.pause();
+        message = `Current music ${queue.currentTrack.title} paused ✅`;
+    }
 
-    return inter.editReply({ content: `${success ? `Current music ${queue.currentTrack.title} paused ✅` : `Current music ${queue.currentTrack.title} resumed ✅`}`, ephemeral: true});
+    return inter.editReply({
+        content: message, ephemeral: true
+    });
 }
