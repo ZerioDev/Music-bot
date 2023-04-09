81921d883fb9357f65289cea38be1c9a7103b3a8
Niek Vlam <vlam4695@gmail.com>
2023-04-10 01:07:01 +0200

    Fixed missing try/catch block for lyrics button

--- src/buttons/lyrics.js
+++ src/buttons/lyrics.js
@@ -1,8 +1,9 @@
 const { EmbedBuilder } = require('discord.js');
-module.exports = async ({ client, inter, queue }) => { 
+module.exports = async ({ client, inter, queue }) => {
     if (!queue || !queue.isPlaying()) return inter.editReply({ content: `No music currently playing... try again ? ❌`, ephemeral: true });
-
-    const search = await genius.songs.search(queue.currentTrack.title); 
+    
+    try {
+        const search = await genius.songs.search(queue.currentTrack.title);
 
         const song = search.find(song => song.artist.name.toLowerCase() === queue.currentTrack.author.toLowerCase());
         if (!song) return inter.editReply({ content: `No lyrics found for ${queue.currentTrack.title}... try again ? ❌`, ephemeral: true });
@@ -15,8 +16,11 @@ module.exports = async ({ client, inter, queue }) => {
                 .setDescription(toSend)
                 .setColor('#2f3136')
                 .setTimestamp()
-                .setFooter({ text: 'Music comes first - Made with heart by Zerio ❤️', iconURL: inter.member.avatarURL({ dynamic: true })})
-                );
+                .setFooter({ text: 'Music comes first - Made with heart by Zerio ❤️', iconURL: inter.member.avatarURL({ dynamic: true }) })
+            );
         }
-        return inter.editReply({ embeds: embeds, ephemeral: true});
+        return inter.editReply({ embeds: embeds, ephemeral: true });
+    } catch (error) {
+        inter.editReply({ content: `Error! Please contact Developers! | ❌`, ephemeral: true });
+    }
 }
