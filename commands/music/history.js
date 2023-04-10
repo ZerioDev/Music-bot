8e1e55977bb401a9b2efb14bf580398507d90390
Niek Vlam <vlam4695@gmail.com>
2023-04-10 18:23:09 +0200

    Added history command using GuildQueueHistory

---
+++ commands/music/history.js
@@ -0,0 +1,27 @@
+const { EmbedBuilder } = require('discord.js');
+module.exports = {
+    name: 'history',
+    description: 'See the history of the queue',
+    voiceChannel: false,
+
+    async execute({ inter }) {
+        const queue = player.nodes.get(inter.guildId);
+
+        if (!queue || queue.history.tracks.toArray().length == 0) return inter.editReply({ content: `No music has been played yet`, ephemeral: true });
+
+        const tracks = queue.history.tracks.toArray();
+
+        let description = tracks
+            .slice(0, 20)
+            .map((track, index) => { return `**${index + 1}.** ${track.title} (${track.url})` })
+            .join('\r\n\r\n');
+
+        let ClearEmbed = new EmbedBuilder()
+            .setTitle(`History`)
+            .setDescription(description)
+            .setColor('#2f3136')
+
+        inter.editReply({ embeds: [ClearEmbed] });
+
+    },
+};
