24945557f411e1975796b14fbb9a3bf720819fa3
Niek Vlam <vlam4695@gmail.com>
2023-04-11 00:22:28 +0200

    Added fixed version of ytdl-core + debugging command

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const { Player } = require('discord-player');
 const Genius = require("genius-lyrics");
 const { Client, GatewayIntentBits } = require('discord.js');
 
-
 global.client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
@@ -11,13 +10,21 @@ global.client = new Client({
         GatewayIntentBits.GuildVoiceStates,
         GatewayIntentBits.MessageContent
     ],
-   disableMentions: 'everyone',
+    disableMentions: 'everyone',
 });
 
 client.config = require('./config');
 
 global.player = new Player(client, client.config.opt.discordPlayer);
 global.genius = new Genius.Client();
+
+if (process.argv[2]) {
+    global.player.on('debug', async (message) => {
+        // Emitted when the player sends debug info
+        // Useful for seeing what dependencies, extractors, etc are loaded
+        console.log(`General player debug event: ${message}`);
+    });
+}
 
 require('./src/loader');
 
