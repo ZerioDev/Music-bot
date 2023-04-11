7a21e68c23e8b37de09e43e4bec24abd7c89d0c1
Niek Vlam <vlam4695@gmail.com>
2023-04-11 09:55:16 +0200

    Removed debugging

--- main.js
+++ main.js
@@ -18,13 +18,6 @@ client.config = require('./config');
 global.player = new Player(client, client.config.opt.discordPlayer);
 global.genius = new Genius.Client();
 
-if (process.argv[2]) {
-    global.player.on('debug', async (message) => {
-        // Emitted when the player sends debug info
-        // Useful for seeing what dependencies, extractors, etc are loaded
-        console.log(`General player debug event: ${message}`);
-    });
-}
 
 require('./src/loader');
 
