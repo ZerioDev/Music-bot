d74a8fff540f375f6ed0720ff3ead559e553f18f
revot334 <78510325+revot334@users.noreply.github.com>
2023-04-10 22:27:22 -0700

    🐛 bug fixes

--- commands/music/filter.js
+++ commands/music/filter.js
@@ -32,12 +32,12 @@ module.exports = {
 
         const filter = filters.find((x) => x.toLowerCase() === infilter.toLowerCase().toString());
 
-        if (!filter) return inter.editReply({ content: `This filter doesn't exist ${inter.member}... try again ? ❌\n${actualFilter ? `Filter currently active ${actualFilter}.\n` : ''}List of available filters ${filters.map(x => `**${x}**`).join(', ')}.`, ephemeral: true });
+        if (!filter) return inter.editReply({ content: `This filter doesn't exist ${inter.member}... try again ? ❌\n${actualFilter ? `Filter currently active ${actualFilter}.\n` : ''}List of available filters ${filters.map(x => `${x}`).join(', ')}.`, ephemeral: true });
 
         await queue.filters.ffmpeg.toggle(filter)
 
         const FilterEmbed = new EmbedBuilder()
-        .setAuthor({name: `The filter ${filter} is now **${queue.filters.ffmpeg.isEnabled(filter) ? 'enabled' : 'disabled'}** ✅\n*Reminder the longer the music is, the longer this will take.*`})
+        .setAuthor({name: `The filter ${filter} is now ${queue.filters.ffmpeg.isEnabled(filter) ? 'enabled' : 'disabled'} ✅\n*Reminder the longer the music is, the longer this will take.*`})
         .setColor('#2f3136')
 
        return inter.editReply({ embeds: [FilterEmbed] });
