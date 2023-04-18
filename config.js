46fd06411464e6e675e42a2501f4e435f9c1d634
jedidavid <jeremydb.david@gmail.com>
2023-04-19 04:15:32 +0800

    feat: merge/update command

--- config.js
+++ config.js
@@ -1,32 +1,33 @@
+require("dotenv").config();
+
 module.exports = {
-    app: {
-        token: 'xxx',
-        playing: 'by Zerio ❤️',
-        global: true,
-        guild: 'xxx',
-        ExtraMessages: false,
-        loopMessage: false,
+  app: {
+    token: "xxx",
+    playing: "by Zerio ❤️",
+    global: true,
+    guild: "xxx",
+    ExtraMessages: false,
+    loopMessage: false,
+  },
 
+  opt: {
+    DJ: {
+      enabled: false,
+      roleName: "",
+      commands: [],
     },
-
-    opt: {
-        DJ: {
-            enabled: false,
-            roleName: '',
-            commands: []
-        },
-        maxVol: 100,
-        spotifyBridge: true,
-        volume: 75,
-        leaveOnEmpty: true,
-        leaveOnEmptyCooldown: 30000,
-        leaveOnEnd: true,
-        leaveOnEndCooldown: 30000,
-        discordPlayer: {
-            ytdlOptions: {
-                quality: 'highestaudio',
-                highWaterMark: 1 << 25
-            }
-        }
-    }
+    maxVol: 100,
+    spotifyBridge: true,
+    volume: 75,
+    leaveOnEmpty: true,
+    leaveOnEmptyCooldown: 30000,
+    leaveOnEnd: true,
+    leaveOnEndCooldown: 30000,
+    discordPlayer: {
+      ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25,
+      },
+    },
+  },
 };
