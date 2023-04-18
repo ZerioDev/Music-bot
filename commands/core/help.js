5b61b794777d215dc9fec6d788345d4dd09025e8
jedidavid <jeremydb.david@gmail.com>
2023-04-19 04:13:42 +0800

    feat: merge/update command

--- commands/core/help.js
+++ commands/core/help.js
@@ -1,21 +1,34 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-    name: 'help',
-    description: "All the commands this bot has!",
-    showHelp: false,
+  name: "help",
+  description: "All the commands this bot has!",
+  showHelp: false,
 
-    execute({ client, inter }) {
-        const commands = client.commands.filter(x => x.showHelp !== false);
+  execute({ client, inter }) {
+    const commands = client.commands.filter((x) => x.showHelp !== false);
 
-        const embed = new EmbedBuilder()
-        .setColor('#ff0000')
-        .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }) })
-        .setDescription('This code comes from a GitHub project [ZerioDev/Music-bot](https://github.com/ZerioDev/Music-bot).\nThe use of this one is possible while keeping the credits for free.\nIf you want to remove the credits join the Discord support server. [here](https://discord.gg/5cGSYV8ZZj)')
-        .addFields([ { name: `Enabled - ${commands.size}`, value: commands.map(x => `\`${x.name}\``).join(' | ') } ])
-        .setTimestamp()
-        .setFooter({ text: 'Music comes first - Made with heart by Zerio ❤️', iconURL: inter.member.avatarURL({ dynamic: true })});
+    const embed = new EmbedBuilder()
+      .setColor("#ff0000")
+      .setAuthor({
+        name: client.user.username,
+        iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }),
+      })
+      .setDescription(
+        "This code comes from a GitHub project [ZerioDev/Music-bot](https://github.com/ZerioDev/Music-bot).\nThe use of this one is possible while keeping the credits for free.\nIf you want to remove the credits join the Discord support server. [here](https://discord.gg/5cGSYV8ZZj)"
+      )
+      .addFields([
+        {
+          name: `Enabled - ${commands.size}`,
+          value: commands.map((x) => `\`${x.name}\``).join(" | "),
+        },
+      ])
+      .setTimestamp()
+      .setFooter({
+        text: "Music comes first - Made with heart by Zerio ❤️",
+        iconURL: inter.member.avatarURL({ dynamic: true }),
+      });
 
-        inter.editReply({ embeds: [embed] });
-    },
+    inter.editReply({ embeds: [embed] });
+  },
 };
