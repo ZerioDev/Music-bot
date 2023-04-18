28a248d56409814b537b535153ca1bbff512eb03
jedidavid <jeremydb.david@gmail.com>
2023-04-19 04:16:07 +0800

    feat: merge/update command

---
+++ events/ready.js
@@ -0,0 +1,6 @@
+module.exports = async (client) => {
+  console.log(
+    `Logged to the client ${client.user.username}\n-> Ready on ${client.guilds.cache.size} servers for a total of ${client.users.cache.size} users`
+  );
+  client.user.setActivity(client.config.app.playing);
+};
