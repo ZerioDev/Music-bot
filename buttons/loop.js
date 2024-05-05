c6625a8d7572ae2e8212d3b2e6266dcf9283edfb
iGameCreep <gamecreep35@gmail.com>
2024-05-05 09:45:40 +0200

    Fix loop button & README.md

--- buttons/loop.js
+++ buttons/loop.js
@@ -4,7 +4,7 @@ module.exports = async ({ inter, queue }) => {
     const methods = ['disabled', 'track', 'queue'];
     if (!queue?.isPlaying()) return inter.editReply({ content: `No music currently playing... try again ? ❌` });
 
-    if (queue.repeatMode === 0) queue.setRepeatMode(QueueRepeatMode.OFF)
+    if (queue.repeatMode === 2) queue.setRepeatMode(QueueRepeatMode.OFF)
     else queue.setRepeatMode(queue.repeatMode + 1)
 
     return inter.editReply({ content: `Loop made has been set to **${methods[queue.repeatMode]}**.✅` });
