2cc16e934fef96b2b5ebe138a4e45a1e74db52ef
BoredKevin <107290249+BoredKevin@users.noreply.github.com>
2024-06-19 16:16:02 +0700

    Update help.js

--- commands/core/help.js
+++ commands/core/help.js
@@ -12,7 +12,7 @@ module.exports = {
         const embed = new EmbedBuilder()
             .setColor('#ff0000')
             .setAuthor({ name: client.user.username, iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }) })
-            .setDescription(await Translate('This code comes from a <GitHub> project <[ZerioDev/Music-bot](https://github.com/ZerioDev/Music-bot)>.<\n>The use of this one is possible while keeping the credits for free.<\n>If you want to remove the credits join the Discord support server. <[here](https://discord.gg/5cGSYV8ZZj)>)'))
+            .setDescription(await Translate('Lethal Boombox is a simple open-source Discord music bot for playing and managing your favorite tunes with ease. <\n>Originally from <[ZerioDev/Music-bot](https://github.com/ZerioDev/Music-bot)>.'))
             .addFields([{ name: `Enabled - ${commands.size}`, value: commands.map(x => `\`${x.name}\``).join(' | ') }])
             .setTimestamp()
             .setFooter({ text: await Translate('Music comes first - Made with heart by the Community <❤️>'), iconURL: inter.member.avatarURL({ dynamic: true }) });
