d91e84bbf463101c4b70a27d528cff393722d585
BoredKevin <107290249+BoredKevin@users.noreply.github.com>
2024-06-22 18:42:47 +0700

    remove "requested by"
    because broken for some reason

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -16,12 +16,12 @@ module.exports = {
         const methods = ['', '🔁', '🔂'];
         const songs = queue.tracks.size;
         const nextSongs = songs > 5 ? await Translate(`and <${songs - 5}> other song(s)...`) : await Translate(`<${songs}> song(s) in the playlist...`);
-        const tracks = queue.tracks.map((track, i) => `${i + 1} - ${track.title} | ${track.author} (requested by : ${track.requestedBy ? track.requestedBy.displayName : "unknown"})`);
+        const tracks = queue.tracks.map((track, i) => `${i + 1} | ${track.author} - ${track.title}})`); //| (requested by : ${track.requestedBy ? track.requestedBy.displayName : "unknown"})`);
         const embed = new EmbedBuilder()
             .setColor('#2f3136')
             .setThumbnail(inter.guild.iconURL({ size: 2048, dynamic: true }))
             .setAuthor({ name: await Translate(`Server queue - <${inter.guild.name}> <${methods[queue.repeatMode]}>`), iconURL: client.user.displayAvatarURL({ size: 1024, dynamic: true }) })
-            .setDescription(await Translate(`Current <${queue.currentTrack.title}> <\n\n> <${tracks.slice(0, 5).join('\n')}> <\n\n> <${nextSongs}>`))
+            .setDescription(await Translate(`Currently playing <${queue.currentTrack.title}> <\n\n> <${tracks.slice(0, 5).join('\n')}> <\n\n> <${nextSongs}>`))
             .setTimestamp()
             .setFooter({ text: await Translate('Music comes first - Made with <❤️> by BoredKevin'), iconURL: inter.member.avatarURL({ dynamic: true }) });
 
