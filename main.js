992e98821c4ad812c0d718a57bd38ecd02adccbf
shashwat <shashwatmukul2007@gmail.com>
2024-10-23 22:00:37 +0530

    Fix YouTube extractor issue by registering YoutubeiExtractor

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-require('dotenv').config()
+require('dotenv').config(); // Load environment variables
 
 const { Player } = require('discord-player');
 const { Client, GatewayIntentBits } = require('discord.js');
+const { YoutubeiExtractor } = require('discord-player-youtubei'); // Import the new extractor
 
 global.client = new Client({
     intents: [
@@ -9,7 +10,7 @@ global.client = new Client({
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildVoiceStates,
-        GatewayIntentBits.MessageContent
+        GatewayIntentBits.MessageContent,
     ],
     disableMentions: 'everyone',
 });
@@ -17,18 +18,16 @@ global.client = new Client({
 client.config = require('./config');
 
 const player = new Player(client, client.config.opt.discordPlayer);
-player.extractors.loadDefault();
+// Register the new Youtubei extractor
+player.extractors.register(YoutubeiExtractor, {});
 
-console.clear()
+console.clear();
 require('./loader');
 
-client.login(client.config.app.token)
-.catch(async (e) => {
-    if(e.message === 'An invalid token was provided.'){
-    require('./process_tools')
-    .throwConfigError('app', 'token', '\n\t   ❌ Invalid Token Provided! ❌ \n\tchange the token in the config file\n')}
-
-    else{
-        console.error('❌ An error occurred while trying to login to the bot! ❌ \n', e)
+client.login(client.config.app.token).catch(async (e) => {
+    if (e.message === 'An invalid token was provided.') {
+        require('./process_tools').throwConfigError('app', 'token', '\n\t   ❌ Invalid Token Provided! ❌ \n\tChange the token in the config file\n');
+    } else {
+        console.error('❌ An error occurred while trying to login to the bot! ❌ \n', e);
     }
 });
